98cfc0d6fe009b53cff586c00524146cdffa511d
markmumba <markmumba01@gmail.com>
2019-09-24 17:03:56 +0300

    my leavepage

--- sanergy_leave/models.py
+++ sanergy_leave/models.py
@@ -1,19 +1,6 @@
 import datetime
 from django.db import models
-from django.utils import timezone
-from enumchoicefield import ChoiceEnum, EnumChoiceField
+
+
 
 # Create your models here.
-
-class workerChoices(ChoiceEnum):
-    Male = 'M'
-    Female = 'F'
-    # worker level
-    Employee = 'Employee'
-    Manager = 'Manager'
-    HR = 'HR'
-    # employment status
-    Permanent = 'Permanent'
-    Probationary = 'Probationary'
-    Limited_term = 'Limited-term'
-    Temporary = 'Temporary'
