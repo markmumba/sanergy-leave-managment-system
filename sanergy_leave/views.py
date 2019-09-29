29ccec58b00f66130dc6f1f4756afafd2fbb5553
markmumba <markmumba01@gmail.com>
2019-09-29 14:47:09 +0300

    the apply form is done

--- sanergy_leave/views.py
+++ sanergy_leave/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render,redirect
-from django.http import HttpResponse,Http404,HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from .forms import *
+from datetime import date
 # Create your views here.
+
+
 def homepage(request):
     return render(request, 'sanergytemplates/homepage.html')
 
@@ -11,20 +14,22 @@ def apply_leave(request):
     current_user = request.user
     if request.method == 'POST':
         form = LeaveForm(request.POST, request.FILES)
-        if form.is_valid():
-            leave=form.save(commit = False)
-            leave.username = current_user
-            # leave.save()
-            print("Form submitted")
-        
-        return redirect('applyform')
-        
+        if 'Begin_Date' < 'End_Date':
+            delta = 'Begin-Date'-'End_date'
+            Requested_days = delta.days
+
+            if form.is_valid():
+                leave = form.save(commit=False)
+                leave.username = current_user
+
+            return redirect('applyform')
+
     else:
         form = LeaveForm()
-        
-    leaves = Leave.print_all()
 
-    return render(request, 'sanergytemplates/leave_apply.html',{'lform':form, "leavess":leaves})
+        leaves = Leave.print_all()
+
+        return render(request, 'sanergytemplates/leave_apply.html', {'lform': form, "leavess": leaves})
 
 
 # def append_to_table (request):
