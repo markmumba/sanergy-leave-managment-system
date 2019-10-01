9af1300dc906db81c8c61092fa9d69b539c33567
markmumba <markmumba01@gmail.com>
2019-10-01 08:58:48 +0300

    the admin page

--- sanergy_leave/email.py
+++ sanergy_leave/email.py
@@ -2,4 +2,11 @@ from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
 
-def acceptance email
+def acceptance_email(name,receiver):
+    subject= 'Leave has been accepted'
+    sender ='sprovider549@gmail.com'
+
+    text _content = render_to_string('email/accepted.txt')
+    html_content = render_to_string('email/accepted.html')
+
+    msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
