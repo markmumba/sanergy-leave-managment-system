6432c17038a11aa04163d9598d1f2bdf1a35844c
markmumba <markmumba01@gmail.com>
2019-10-08 21:35:13 +0300

    making the changes to the ui

--- sanergy_leave/email.py
+++ sanergy_leave/email.py
@@ -16,12 +16,12 @@ def welcome_email(name,receiver, password):
 
 # status approval/declined emails
 
-def status_approval_email(name,receiver):
+def status_approval_email(name,receiver,date,date2):
     subject= 'Leave has been accepted'
     sender ='sprovider549@gmail.com'
 
-    text_content = render_to_string('email/accepted.txt')
-    html_content = render_to_string('email/accepted.html')
+    text_content = render_to_string('email/accepted.txt',{"name":name,"date":date,"date2":date2})
+    html_content = render_to_string('email/accepted.html',{"name":name,"date":date,"date2":date2})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
@@ -32,7 +32,7 @@ def status_declined_email(name,receiver):
     subject='Leave application has been rejected'
     sender = 'sprovider549@gmail.com'
 
-    text_content = render_to_string('email/declined.txt')
+    text_content = render_to_string('email/declined.txt',{"name": name})
     html_content = render_to_string('email/declined.html')
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
